2c10ca9fc12b672221744fd4b36e1029edb6e427
Patrick Pircher <patrick.pircher@ibm.com>
2019-05-02 11:01:56 +0200

    memory leak example

---
+++ app/App_Resources/Android/src/main/java/my/Test.java
@@ -0,0 +1,30 @@
+package my.Test;
+
+public class Test {
+    private java.util.Timer timer = new java.util.Timer();
+    public void run() {
+        Test test = this;
+        java.util.TimerTask task = new java.util.TimerTask() {
+            @Override
+            public void run() {
+                test.invoke();
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 5);
+    }
+
+    public void invoke() {
+
+    }
+
+    public byte[] getValue() {
+        String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder builder = new StringBuilder();
+        int count = 50;
+        while (count-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString().getBytes();
+    }
+}
